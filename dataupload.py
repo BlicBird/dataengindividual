8b3fff509e0f180f91e8c20e00cf000737ff3634
BlicBird <danielsantosh@hotmail.co.uk>
2022-04-19 13:43:07 +0000

    dataupload second commit

--- dataupload.py
+++ dataupload.py
@@ -33,9 +33,6 @@ spark = SparkSession \
     .getOrCreate()
     
 
-sc = pyspark.SparkContext.getOrCreate()
-sqlContext = SQLContext(sc)
-
 players=spark.read.parquet("players_files")
 player_info=spark.read.parquet("player_info_files")
 stats=spark.read.parquet("stats_files")
@@ -43,50 +40,50 @@ player_personal=spark.read.parquet("player_personal_files")
 tweet_counts= spark.read.parquet("tweet_counts_files")
 tweets = spark.read.parquet("tweets_files")
 
-players.write.jdbc(url = "jdbc:postgresql://depgdb.crhso94tou3n.eu-west-2.rds.amazonaws.com:5432/danielsantosh17", 
+players.write.jdbc(url = "jdbc:postgresql://football1.cgjkytjtagwe.eu-west-2.rds.amazonaws.com:5432/football1", 
 table = "football.players", 
 mode = "append", 
-properties = {"user":"danielsantosh17", 
+properties = {"user":"postgres", 
 "partition": "1",
 "password": "qwerty123", 
 "driver": "org.postgresql.Driver" })
 
-player_info.write.jdbc(url = "jdbc:postgresql://depgdb.crhso94tou3n.eu-west-2.rds.amazonaws.com:5432/danielsantosh17", 
+player_info.write.jdbc(url = "jdbc:postgresql://football1.cgjkytjtagwe.eu-west-2.rds.amazonaws.com:5432/football1", 
 table = "football.player_info", 
 mode = "append", 
-properties = {"user":"danielsantosh17", 
+properties = {"user":"postgres", 
 "partition": "1",
 "password": "qwerty123", 
 "driver": "org.postgresql.Driver" })
 
-stats.write.jdbc(url = "jdbc:postgresql://depgdb.crhso94tou3n.eu-west-2.rds.amazonaws.com:5432/danielsantosh17", 
+stats.write.jdbc(url = "jdbc:postgresql://football1.cgjkytjtagwe.eu-west-2.rds.amazonaws.com:5432/football1", 
 table = "football.stats", 
 mode = "append", 
-properties = {"user":"danielsantosh17", 
+properties = {"user":"postgres", 
 "partition": "1",
 "password": "qwerty123", 
 "driver": "org.postgresql.Driver" })
 
-player_personal.write.jdbc(url = "jdbc:postgresql://depgdb.crhso94tou3n.eu-west-2.rds.amazonaws.com:5432/danielsantosh17", 
+player_personal.write.jdbc(url = "jdbc:postgresql://football1.cgjkytjtagwe.eu-west-2.rds.amazonaws.com:5432/football1", 
 table = "football.player_personal", 
 mode = "append", 
-properties = {"user":"danielsantosh17", 
+properties = {"user":"postgres", 
 "partition": "1",
 "password": "qwerty123", 
 "driver": "org.postgresql.Driver" })
 
-tweet_counts.write.jdbc(url = "jdbc:postgresql://depgdb.crhso94tou3n.eu-west-2.rds.amazonaws.com:5432/danielsantosh17", 
+tweet_counts.write.jdbc(url = "jdbc:postgresql://football1.cgjkytjtagwe.eu-west-2.rds.amazonaws.com:5432/football1", 
 table = "football.tweet_counts", 
 mode = "append", 
-properties = {"user":"danielsantosh17", 
+properties = {"user":"postgres", 
 "partition": "1",
 "password": "qwerty123", 
 "driver": "org.postgresql.Driver" })
 
-tweets.write.jdbc(url = "jdbc:postgresql://depgdb.crhso94tou3n.eu-west-2.rds.amazonaws.com:5432/danielsantosh17", 
+tweets.write.jdbc(url = "jdbc:postgresql://football1.cgjkytjtagwe.eu-west-2.rds.amazonaws.com:5432/football1", 
 table = "football.tweets", 
 mode = "append", 
-properties = {"user":"danielsantosh17", 
+properties = {"user":"postgres", 
 "partition": "1",
 "password": "qwerty123", 
 "driver": "org.postgresql.Driver" })
